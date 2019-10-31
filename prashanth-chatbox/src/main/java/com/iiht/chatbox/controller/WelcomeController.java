d82e9bd0a1ed2d574793f3c0d18776eaec86f66f
JALA PRASHANTH <JALA PRASHANTH@192.168.11.20>
2019-10-31 10:43:44 +0530

    Commit Prashanth

---
+++ prashanth-chatbox/src/main/java/com/iiht/chatbox/controller/WelcomeController.java
@@ -0,0 +1,16 @@
+package com.iiht.chatbox.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class WelcomeController {
+	@RequestMapping(value="/" ,method=RequestMethod.GET)
+	public String home()
+	{
+		return  "home";
+		
+	}
+
+}
